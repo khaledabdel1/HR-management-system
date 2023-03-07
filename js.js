33e9ef517fd251131604c9700e9f2ba0b8ac9ccc
khaledabdel1 <klda3@yahoo.com>
2023-03-07 14:20:30 +0000

    new

--- js.js
+++ js.js
@@ -1,54 +1,54 @@
 'strict mode';
-const khaled = [];
+const allemployee = [];
 
-function Employee (employeeId,fullName,department,level,imageURL){
+function Employee (employeeId,fullName,department,level,imageUrl){
     this.employeeId = employeeId;
     this.fullName = fullName,
     this.department = department,
     this.Level = level,
-    this.imageURL = imageURL,
-    
-    khaled.push(this);
+    this.imageUrl = imageUrl,
+    allemployee.push(this);
 }
 
 Employee.prototype.salary = function (){
     if (this.level == "Senior") {
         this.salary = Math.floor(Math.random() * (2000 - 1500) ) + 1500;
-        this.netsalary = Math.floor(this.salary - (this.salary * 0.075));
-        return this.netsalary ;
+        
+        return this.salary ;
     } else if (this.level == "Mid-Senior"){
         this.salary = Math.floor(Math.random() * (1500 - 1000) ) + 1000;
-        this.netsalary = Math.floor(this.salary - (this.salary * 0.075));
-        return this.netsalary ;
+        
+        return this.salary ;
     } else if (this.level == "Junior"){
         this.salary = Math.floor(Math.random() * (1000 - 500) ) + 500;
-        this.netsalary = Math.floor(this.salary - (this.salary * 0.075));
-        return this.netsalary ;
+        ;
+        return this.salary ;
 }
 }
 
-const ghaziSamer  = new Employee(1000,"Ghazi Samer" , "Administration" , "Senior" );
+let ghaziSamer  = new Employee(1000,"Ghazi Samer" , "Administration" , "Senior" );
 
-const lanaAli  = new Employee(1001,"Lana Ali" , "Finance" ,"Junior" );
+let lanaAli  = new Employee(1001,"Lana Ali" , "Finance" ,"Junior" );
 
-const tamaraAyoub = new Employee(1002,"Tamara Ayoub" , "Marketing" ,"Senior" );
+let tamaraAyoub = new Employee(1002,"Tamara Ayoub" , "Marketing" ,"Senior" );
 
-const safiWalid = new Employee(1003,"Safi Walid" , "Administration" , "Mid-Senior");
+let safiWalid = new Employee(1003,"Safi Walid" , "Administration" , "Mid-Senior");
 
-const omarZaid = new Employee(1004,"Omar Zaid" , "Development" ,"Senior" );
+let omarZaid = new Employee(1004,"Omar Zaid" , "Development" ,"Senior" );
 
-const ranaSaleh = new Employee(1005,"Rana Saleh" , "Development" ,"Junior" );
+let ranaSaleh = new Employee(1005,"Rana Saleh" , "Development" ,"Junior" );
 
-const hadiAhmad = new Employee(1006,"Hadi Ahmad" , "Finance" , "Mid-Senior" );
+let hadiAhmad = new Employee(1006,"Hadi Ahmad" , "Finance" , "Mid-Senior" );
 
-Employee.prototype.showInfo=function() {
-    document.write( `Emplyee Name is : ${this.fullName},and his Salary is :  ${this.salary()} <br>`);
+Employee.prototype.rednder=function() {
+    document.write( `<h1>Emplyee : ${this.fullName}, salary: ${this.salary()}<h1>`);
 }
 
-let table =  `<table border = 1> <tr> <th>ID</th><th>Name</th> <th>Salary</th><th>Department</th><th>Level</th> </tr>` ;
-for (const key in khaled) {
-
-    table+=`<tr><td>${khaled[key].emplyeeId}</td> <td>${khaled[key].fullName}</td> <td>${khaled[key].salary()}</td><td>${khaled[key].department}</td><td>${khaled[key].level}</td></tr>`;
-
-}
-document.write(table);
+ghaziSamer.rednder()
+lanaAli.rednder() 
+tamaraAyoub.rednder()
+safiWalid.rednder()
+omarZaid.rednder()
+ranaSaleh.rednder()
+hadiAhmad.rednder()
+ghaziSamer.salary()  
