0b190c837e8e845a95784260230f5b77587c66af
khaledabdel1 <klda3@yahoo.com>
2023-03-08 16:36:07 +0000

    updet

--- js.js
+++ js.js
@@ -1,54 +1,45 @@
-'strict mode';
-const allemployee = [];
-
-function Employee (employeeId,fullName,department,level,imageUrl){
+'stricy mode';
+const allEmployee = [];
+console.log(allEmployee)
+function Employee(employeeId,fullName,Department,Level,imageUrl){
     this.employeeId = employeeId;
-    this.fullName = fullName,
-    this.department = department,
-    this.Level = level,
-    this.imageUrl = imageUrl,
-    allemployee.push(this);
+    this.fullName = fullName;
+    this.Department = Department;
+    this.Level = Level;
+    this.imageUrl = imageUrl
+    this.Salary = 0
+    allEmployee.push(this)
+} 
+
+let GhaziSamer = new Employee("1000","Ghazi Samer","Administration","Senior",);
+let LanaAli = new Employee("1001","Lana Ali","Finance","Senior",);
+let TamaraAyoub = new Employee("1002","Tamara Ayoub","Marketing","Senior",);
+let SafiWalid = new Employee("1003","Safi Walid","Administration","Mid-Senior",);
+let OmarZaid = new Employee("1004","Omar Zaid","Development","Senior",);
+let RanaSaleh = new Employee("1005","Rana Saleh","Development","Junior",);
+let HadiAhmad = new Employee("1006","Hadi Ahmad","Finance","Mid-Senior",);
+
+Employee.prototype.salaryCalc = function (){
+        if(this.level=='Junior'){
+            this.salary = Math.floor(Math.random() * (1000 - 500) ) + 500;
+        }
+        else if(this.level=='midSenior'){
+            this.salary = Math.floor(Math.random() * (1500 - 1000) ) + 1000;
+        }
+        else {
+            this.salary = Math.floor(Math.random() * (2000 - 1500) ) + 1500;
+        }
+        this.salary = this.salary * (1-0.075)
+        }
+
+Employee.prototype.render = function (){
+    document.write (`<h1>Name is : ${this.fullName} and his salary is ${this.salary}</h1>` )
 }
 
-Employee.prototype.salary = function (){
-    if (this.level == "Senior") {
-        this.salary = Math.floor(Math.random() * (2000 - 1500) ) + 1500;
-        
-        return this.salary ;
-    } else if (this.level == "Mid-Senior"){
-        this.salary = Math.floor(Math.random() * (1500 - 1000) ) + 1000;
-        
-        return this.salary ;
-    } else if (this.level == "Junior"){
-        this.salary = Math.floor(Math.random() * (1000 - 500) ) + 500;
-        ;
-        return this.salary ;
+function all() {
+for (i = 0; i < allEmployee.length; i++) {
+    allEmployee[i].salaryCalc();
+    allEmployee[i].render();
 }
 }
-
-let ghaziSamer  = new Employee(1000,"Ghazi Samer" , "Administration" , "Senior" );
-
-let lanaAli  = new Employee(1001,"Lana Ali" , "Finance" ,"Junior" );
-
-let tamaraAyoub = new Employee(1002,"Tamara Ayoub" , "Marketing" ,"Senior" );
-
-let safiWalid = new Employee(1003,"Safi Walid" , "Administration" , "Mid-Senior");
-
-let omarZaid = new Employee(1004,"Omar Zaid" , "Development" ,"Senior" );
-
-let ranaSaleh = new Employee(1005,"Rana Saleh" , "Development" ,"Junior" );
-
-let hadiAhmad = new Employee(1006,"Hadi Ahmad" , "Finance" , "Mid-Senior" );
-
-Employee.prototype.rednder=function() {
-    document.write( `<h1>Emplyee : ${this.fullName}, salary: ${this.salary()}<h1>`);
-}
-
-ghaziSamer.rednder()
-lanaAli.rednder() 
-tamaraAyoub.rednder()
-safiWalid.rednder()
-omarZaid.rednder()
-ranaSaleh.rednder()
-hadiAhmad.rednder()
-ghaziSamer.salary()  
+all()
