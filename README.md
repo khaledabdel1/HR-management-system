0439557ea66da9cc437a3d908cc52c7301b52faa
khaledabdel1 <klda3@yahoo.com>
2023-03-10 15:24:15 +0000

    event

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # HR-management-system
+
+9/3/2023
+Separate the employees cards by their department where you will have a separate section for each department.
--- js.js
+++ js.js
@@ -1,14 +1,21 @@
 'stricy mode';
+let sectionEl = document.getElementById("org")
+sectionEl.width = 20
+sectionEl.style.backgroundColor =  "#539165";
+console.log(sectionEl)
 const allEmployee = [];
 console.log(allEmployee)
-function Employee(employeeId,fullName,Department,Level,imageUrl){
-    this.employeeId = employeeId;
+
+function Employee(fullName,Department,Level,imageUrl,employeeId,){
+    
     this.fullName = fullName;
     this.Department = Department;
     this.Level = Level;
-    this.imageUrl = imageUrl
+    this.imageUrl = imageUrl;
+    this.employeeId = employeeId;
     this.Salary = 0
     allEmployee.push(this)
+
 } 
 
 let GhaziSamer = new Employee("1000","Ghazi Samer","Administration","Senior",);
@@ -42,4 +49,52 @@ for (i = 0; i < allEmployee.length; i++) {
     allEmployee[i].render();
 }
 }
-all()
+//all()
+    function employeeIdCalc(){
+    return Math.floor(1000 + Math.random() * 9000);
+   
+}
+
+Employee.prototype.newrender = function() {
+    let imgE1 = document.createElement('img');
+    imgE1.src = this.imageUrl;
+    imgE1.width = 200
+    imgE1.height = 200
+    sectionEl.appendChild(imgE1);
+
+    let pE1 = document.createElement('h5');
+    pE1.textContent = `Name: ${this.fullName} -ID : ${employeeIdCalc ()}`;
+    pE1.style.color= "#FFFFFF";
+    sectionEl.appendChild(pE1);
+    
+    
+    let h5E1 = document.createElement('h5');
+    h5E1.textContent = `Department: ${this.Department} - Level : ${this.Level} `
+    h5E1.style.color= "#FFFFFF";
+    sectionEl.appendChild(h5E1)
+
+    let h5E2 = document.createElement('h5');
+    h5E2.textContent = `${this.Salary} `
+    h5E2.style.color= "#FFFFFF";
+    sectionEl.appendChild(h5E2)
+  
+}
+    
+
+    
+let foemIe = document.getElementById("fullname");
+foemIe.addEventListener("submit",submitHolder);
+
+function submitHolder (event){
+    event.preventDefault();
+    let eFullName = event.target.name.value;
+    let eImage = event.target.image.value;
+    
+    let eDepartment = event.target.Department.value;
+    let eLevel = event.target.level.value;
+    
+    
+    let newEmployee = new Employee(eFullName,eDepartment,eLevel,eImage)
+    newEmployee.newrender()
+    
+}
